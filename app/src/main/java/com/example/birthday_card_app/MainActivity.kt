d0f1577d04e9f22b4a56abffa19180625462b18c
mad-skull <da.madskull@gmail.com>
2021-08-28 00:45:28 +0530

    Creating new activity

--- app/src/main/java/com/example/birthday_card_app/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card_app/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.birthday_card_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun createBirthdayCard(view: android.view.View) {
+
+        var name = nameEdit.editableText.toString()
+        Toast.makeText(context:this, text:"name is $name", Toast.LENGTH_LONG).show()
     }
 }
