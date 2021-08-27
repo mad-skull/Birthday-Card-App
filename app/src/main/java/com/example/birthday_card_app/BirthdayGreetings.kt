f3a73271c825178610e1852a41cd92763cf1cd3d
mad-skull <da.madskull@gmail.com>
2021-08-28 01:23:31 +0530

    Activity Update

--- app/src/main/java/com/example/birthday_card_app/BirthdayGreetings.kt
+++ app/src/main/java/com/example/birthday_card_app/BirthdayGreetings.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class BirthdayGreetings : AppCompatActivity() {
+
+    companion object {
+        const val = NAME_EXTRA = "name extra"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greetings)
+
+        val name = intent.getStringArrayExtra(NAME_EXTRA)
+        BirthdayGreetings.text = "Happy Birthday $name"
     }
 }
--- app/src/main/java/com/example/birthday_card_app/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.birthday_card_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,8 +14,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun createBirthdayCard(view: android.view.View) {
-
-        var name = nameEdit.editableText.toString()
-        Toast.makeText(context:this, text:"name is $name", Toast.LENGTH_LONG).show()
+        val name = nameInput.editableText.toString()
+        val intent = Intent(this, BirthdayGreetings::class.java)
+        intent.putExtra(BirthdayGreetings.NAME_EXTRA, name)
+        startActivity(intent)
     }
 }
